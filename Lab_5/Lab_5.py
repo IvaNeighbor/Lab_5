448ea3aafe6197743f11d61910afd4006b602458
IvaNeighbor <duzak2006@gmail.com>
2024-09-13 12:50:48 +0300

    final

--- Lab_5/Lab_5.py
+++ Lab_5/Lab_5.py
@@ -1,1 +1,9 @@
-
+n=int(input("Enter size of array:"))
+arr=[int(input(f"Enter elements[{i}]:")) for i in range(n)]
+sum=0
+for i in range(len(arr)):
+    if arr[i]%3==0 and arr[i]>0:
+        sum+=arr[i]
+        print("Sum:", sum)
+    else:
+        print(f"This elements[{i}] dont more than 0 and divisible by 3")
